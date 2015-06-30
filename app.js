0788378c65043504c6552ae623d6c14d0e7141a5
maikpaulus <maikpaulus@web.de>
2015-06-30 23:17:42 +0200

    Second commit, testing remote

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('INES sponsored by MEAN');
+    res.send('INES sponsored by MEAN and Icecream!');
 });
 
 var server = app.listen(3000, function () {
