20f8b8765579de7f1c43d39dde00f5902726fe2a
maikpaulus <maikpaulus@web.de>
2015-08-10 20:30:18 +0200

    Merge branches 'db-api' and 'master' of https://github.com/jsguys/inespresentation
    Conflicts:
    	server.js

--- server.js
+++ server.js
@@ -3,51 +3,27 @@ var http = require('http');
 
 var dbconfig = require('./config/db');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var routes = require('./routes/api');
 
 app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
 
 app.post('/api/:entity', routes.api.create);
 app.get('/api/:entity/:key/:value', routes.api.read);
 app.get('/api/:entity', routes.api.read);
 app.put('/api/:entity/:key/:value', routes.api.update);
-
-app.route('/').get(function (req, res) {
-    factory.getDriver(dbconfig.driver, function (db) {
-        db.connect(dbconfig.host, dbconfig.port, dbconfig.database, dbconfig.user, dbconfig.password);
-
-        db.setEntity(Person);
-        db.crud(db.ACTIONS.READ, {}, function (result) {
-           console.log(1, result);
-        });
-
-        db.setEntity(Presentation);
-        db.crud(db.ACTIONS.READ, {}, function (result) {
-            console.log(2, result);
-        });
-
-        db.setEntity(Person);
-        db.crud(db.ACTIONS.READ, {}, function (result) {
-            console.log(3, result);
-        });
-
-        res.send('root');
-    });
-});
-
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+app.set('port', process.env.port || 3000);
 
 var server = http.createServer(app);
-
 var io = require('./io')(server);
 
 server.listen(app.get('port'), function () {
     console.log('express running on port ' + app.get('port'));
 });
 
-String.prototype.capitalize = function() {
-    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+String.prototype.capitalize = function () {
+    return this.replace(/(?:^|\s)\S/g, function (a) {
+        return a.toUpperCase();
+    });
 };
